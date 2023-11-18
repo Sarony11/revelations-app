ae0dd285a9bd001790c1e0ed0cdf23121f1bff8c
Saul Fernandez <sarony11@gmail.com>
2023-11-18 22:14:17 +0100

    Feat: Add scan packs function

---
+++ lambda/get_packs.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('question_packs')
+
+    # Escanear la tabla
+    try:
+        response = table.scan()
+        items = response['Items']
+        print(items)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(items)
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(e))
+        }
+
+#lambda_handler(None, None)
