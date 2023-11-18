0f2ad20a8dbc9d2fb37c6dd83409fcdd254be38b
Saul Fernandez <sarony11@gmail.com>
2023-11-18 22:36:38 +0100

    Feat: add variable argument for table name

--- data/verify_data.py
+++ data/verify_data.py
@@ -1,7 +1,14 @@
 import boto3
+import sys
+
+if len(sys.argv) < 2:
+    print("Use: python script.py <table_name>")
+    sys.exit(1)
+
+table_name = sys.argv[1]
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('users')
+table = dynamodb.Table(table_name)
 
 response = table.scan()
 
